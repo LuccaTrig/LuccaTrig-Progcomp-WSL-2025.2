511acbdc668396c0ca7684b7eecbb89fbb205e10
lucca <lhtrigueiro@gmail.com>
2025-09-18 02:53:01 -0300

    Atvs

---
+++ atv1-q3.py
@@ -0,0 +1,7 @@
+l = float(input("Digite a largura do terreno:"))
+c = float(input("Digite o comprimento do terreno:"))
+
+perimetro = (l + c) * 2
+area = l * c
+
+print("O perímetro do terreno é", perimetro,"e sua a área é", area)
